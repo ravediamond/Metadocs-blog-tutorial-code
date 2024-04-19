cc5f646a6d554d8a434b04d75f905ff20ffb8084
ravindu.somawansa <ravindu.somawansa@gmail.com>
2024-04-19 16:16:21 +0200

    feat: working complex web app demo

--- create-complex-webapp-with-langchain-alternative-feature/app.py
+++ create-complex-webapp-with-langchain-alternative-feature/app.py
@@ -1,13 +1,19 @@
 import os
+from typing import List, Optional
 
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import ConfigurableField
+from langchain_core.runnables import ConfigurableField, RunnablePassthrough
+from langchain.schema import format_document, Document
+from langchain.schema.runnable import (
+    ConfigurableField,
+    RunnableConfig,
+    RunnableSerializable,
+)
 from langchain_community.vectorstores import FAISS
 from langchain_core.output_parsers import StrOutputParser
-from enum import Enum
 
 
 # Load the variables from .env
@@ -15,16 +21,8 @@ load_dotenv()
 
 st.title("Hello, Metadocs readers!")
 
-
-class Scenario(Enum):
-    ONE_VECTOR_STORE_SINGLE_LINE = "one_vector_store_single_line"
-    ONE_VECTOR_STORE_DETAILED = "one_vector_store_detailed"
-    TWO_VECTOR_STORES_SINGLE_LINE = "two_vector_stores_single_line"
-    TWO_VECTOR_STORES_DETAILED = "two_vector_stores_detailed"
-
-
 # Base templates for vector store prompts
-template_one_store_single_line = PromptTemplate.from_template(
+template_single_line = PromptTemplate.from_template(
     """Answer the question in a single line based on the following context:
 {context}
 
@@ -32,7 +30,7 @@ Question: {question}
 """
 )
 
-template_one_store_detailed = PromptTemplate.from_template(
+template_detailed = PromptTemplate.from_template(
     """Answer the question in a detailed way with an idea per bullet point based on the following context:
 {context}
 
@@ -40,46 +38,20 @@ Question: {question}
 """
 )
 
-base_template_two_stores_single_line = PromptTemplate.from_template(
-    """Answer the question in a single line based on the following contexts:
-{context1}
-
-{context2}
-
-Question: {question}
-"""
-)
-
-base_template_two_stores_detailed = PromptTemplate.from_template(
-    """Answer the question in a detailed way with an idea per bullet based on the following contexts:
-{context1}
-
-{context2}
-
-Question: {question}
-"""
-)
-
-scenario_name = []
-
 prompt_alternatives = {
-    Scenario.ONE_VECTOR_STORE_DETAILED.value: template_one_store_detailed,
-    Scenario.TWO_VECTOR_STORES_SINGLE_LINE.value: base_template_two_stores_single_line,
-    Scenario.TWO_VECTOR_STORES_DETAILED.value: base_template_two_stores_detailed,
+    "detailed": template_detailed,
 }
 
-# Configurable prompt templates with alternatives for detailed and summary answers
 
-configurable_prompt = template_one_store_single_line.configurable_alternatives(
+configurable_prompt = template_single_line.configurable_alternatives(
     which=ConfigurableField(
-        id="scenario",
-        name="Scenario",
-        description="The scenario to use",
+        id="output_type",
+        name="Output type",
+        description="The type for the output, single line or detailed.",
     ),
-    default_key=Scenario.ONE_VECTOR_STORE_SINGLE_LINE.value,
-    **prompt_alternatives
+    default_key="single_line",
+    **prompt_alternatives,
 )
-
 
 model = ChatOpenAI(
     temperature=0,
@@ -88,75 +60,110 @@ model = ChatOpenAI(
 )
 embedding = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_KEY"])
 
-first_retriever = None
-second_retriever = None
+politic_vector_store_path = "politic_vector_store_path.faiss"
+environnetal_vector_store_path = "environnetal_vector_store_path.faiss"
+
+
+class ConfigurableFaissRetriever(RunnableSerializable[str, List[Document]]):
+    vector_store_topic: str
+
+    def invoke(
+        self, input: str, config: Optional[RunnableConfig] = None
+    ) -> List[Document]:
+        """Invoke the retriever."""
+
+        vector_store_path = (
+            politic_vector_store_path
+            if "politic" in vector_store_topic
+            else environnetal_vector_store_path
+        )
+        faiss_vector_store = FAISS.load_local(
+            vector_store_path,
+            embedding,
+            allow_dangerous_deserialization=True,
+        )
+        retriever = faiss_vector_store.as_retriever(
+            search_type="similarity", search_kwargs={"k": 4}
+        )
+        return retriever.invoke(input, config=config)
+
+
+configurable_faiss_vector_store = ConfigurableFaissRetriever(
+    vector_store_topic="politic"
+).configurable_fields(
+    vector_store_topic=ConfigurableField(
+        id="vector_store_topic",
+        name="Vector store topic",
+        description="The topic of the faiss vector store.",
+    )
+)
+
+DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
+
+
+def combine_documents(
+    docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
+):
+    """Combine documents into a single string."""
+    doc_strings = [format_document(doc, document_prompt) for doc in docs]
+    return document_separator.join(doc_strings)
+
 
 tab1, tab2 = st.tabs(["Upload Files", "Query"])
 
 with tab1:
     st.header("Upload Files to Vector Stores")
-    first_index_uploaded_file = st.file_uploader(
-        "Upload a text file to the first vector store:", type="txt", key="first_index"
+    politic_index_uploaded_file = st.file_uploader(
+        "Upload a text file to Politic vector store:", type="txt", key="politic_index"
     )
-    if first_index_uploaded_file is not None:
-        string_data = first_index_uploaded_file.getvalue().decode("utf-8")
+    if politic_index_uploaded_file is not None:
+        string_data = politic_index_uploaded_file.getvalue().decode("utf-8")
         splitted_data = string_data.split("\n\n")
-        first_vectorstore = FAISS.from_texts(splitted_data, embedding=embedding)
-        first_retriever = first_vectorstore.as_retriever()
-        st.success("First vector store loaded successfully!")
+        politic_vectorstore = FAISS.from_texts(splitted_data, embedding=embedding)
+        politic_vectorstore.save_local(politic_vector_store_path)
+        st.success("Politic vector store loaded successfully!")
 
-    second_index_uploaded_file = st.file_uploader(
-        "Upload a text file to the second vector store:", type="txt", key="second_index"
+    environnetal_index_uploaded_file = st.file_uploader(
+        "Upload a text file to the Environnemental vector store:",
+        type="txt",
+        key="environnetal_index",
     )
-    if second_index_uploaded_file is not None:
-        string_data = second_index_uploaded_file.getvalue().decode("utf-8")
+    if environnetal_index_uploaded_file is not None:
+        string_data = environnetal_index_uploaded_file.getvalue().decode("utf-8")
         splitted_data = string_data.split("\n\n")
-        second_vectorstore = FAISS.from_texts(splitted_data, embedding=embedding)
-        second_retriever = second_vectorstore.as_retriever()
-        st.success("Second vector store loaded successfully!")
+        environnetal_vectorstore = FAISS.from_texts(splitted_data, embedding=embedding)
+        environnetal_vectorstore.save_local(environnetal_vector_store_path)
+        st.success("Environnemental vector store loaded successfully!")
 
 with tab2:
     st.header("Query a Vector Store")
-    if first_retriever or second_retriever:
-        vector_store_selection = st.selectbox(
+    if os.path.exists(politic_vector_store_path) or os.path.exists(
+        environnetal_vector_store_path
+    ):
+        vector_store_topic = st.selectbox(
             "Choose the vector store configuration:",
-            ["First only", "Second only", "Both"],
+            ["Politic", "Environnemental"],
         )
         output_type = st.selectbox(
-            "Select the type of response:", ["detailed", "single line"]
+            "Select the type of response:", ["detailed", "single_line"]
         )
 
         question = st.text_input("Input your question:")
         if st.button("Get Answer"):
-            if vector_store_selection == "First only" and output_type == "single line":
-                scenario = Scenario.ONE_VECTOR_STORE_SINGLE_LINE.value
-                context = {"context": first_retriever, "question": question}
 
-            elif vector_store_selection == "Both" and output_type == "detailed":
-                scenario = Scenario.ONE_VECTOR_STORE_DETAILED.value
-                context = {"context": second_retriever, "question": question}
-
-            elif vector_store_selection == "Both" and output_type == "single line":
-                scenario = Scenario.TWO_VECTOR_STORES_SINGLE_LINE.value
-                context = {
-                    "context1": first_retriever,
-                    "context2": second_retriever,
-                    "question": question,
-                }
-            elif vector_store_selection == "Both" and output_type == "detailed":
-                scenario = Scenario.TWO_VECTOR_STORES_DETAILED.value
-                context = {
-                    "context1": first_retriever,
-                    "context2": second_retriever,
-                    "question": question,
-                }
-            else:
-                st.write("There is an error.")
+            context = {
+                "context": configurable_faiss_vector_store | combine_documents,
+                "question": RunnablePassthrough(),
+            }
 
             chain = (
                 context | configurable_prompt | model | StrOutputParser()
-            ).with_config(configurable={"scenario": scenario})
-
+            ).with_config(
+                configurable={
+                    "vector_store_topic": vector_store_topic,
+                    "output_type": output_type,
+                }
+            )
             result = chain.invoke(question)
             st.write(result)
     else:
