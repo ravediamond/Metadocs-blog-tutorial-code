9a30314b00946867e969b2138c0d0564eaecf70d
ravindu.somawansa <ravindu.somawansa@gmail.com>
2024-07-31 14:54:45 +0200

    feat: added working tutorial

--- langserve-openai-api/app/server.py
+++ langserve-openai-api/app/server.py
@@ -1,15 +1,14 @@
 import os
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 from langserve import add_routes
-from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-
 
 app = FastAPI(
     title="Metadocs Langserve Tutorial",
@@ -17,37 +16,29 @@ app = FastAPI(
     description="Spin up a LangServe API for learning purpose",
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-    expose_headers=["*"],
-)
 
-config = {
-    "enable_feedback_endpoint": False,
-}
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
 
-template = """Answer the question as if you were Homer Simpsons.
+
+template = """"Answer the question by first detailing the steps or considerations you think are important for solving it. After explaining each part, provide the final answer.
+If there is any aspect you are unsure about, describe what you know and what remains unclear
 Question: {question}
 """
 
 prompt = ChatPromptTemplate.from_template(template)
 model = ChatOpenAI(
     temperature=0,
-    model_name="gpt-3.5-turbo-0125",
+    model_name="gpt-4o-mini",
     openai_api_key=os.environ["OPENAI_KEY"],
 )
-
-
 chain = prompt | model | StrOutputParser()
 
-add_routes(app, chain, path="/homer-chat", **config)
 
+add_routes(app, chain, path="/chat")
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8000, log_level="error")
+    uvicorn.run(app, host="0.0.0.0", port=8000)
